4930eef3a50bd307d47088d87878e8a46a9e3b35
Chukwuanieze Eric <47712998+Ericchuk@users.noreply.github.com>
2022-07-25 07:34:01 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Drum-Kit
-A challenge from JavaScript 30, challenge 1. on clicking on any of the keyboard alphabet on the keyboard, it produces a sound and lighten the alphabet on the screen
+Started this project with the usual html5, css and javascript.
+The html file has n article page which contains the key container and the key container embody the key divs.
+I used the sans-serif for the font family and used margin to center the key container.
+Javascript - it connects the indidvidual key to the audio file by creating an audio file on clicking the expected key and auto playing them.
+It also has a call function that activate a box shadow and a setTimeout that remove the function after 1ms.
--- script.js
+++ script.js
@@ -2,14 +2,41 @@ let keys = document.querySelectorAll(".eachkey");
 let keysValue = document.querySelectorAll(".eachkey h1")
 window.addEventListener("keydown", function(e){
     // KEY A 
+    function lightOut(){
+        keys[0].style.border = "3px solid black";
+        keys[0].style.transform = "scale(1)";
+        keys[0].style.boxShadow = "none";
+        keys[1].style.border = "3px solid black";
+        keys[1].style.transform = "scale(1)";
+        keys[1].style.boxShadow = "none";
+        keys[2].style.border = "3px solid black";
+        keys[2].style.transform = "scale(1)";
+        keys[2].style.boxShadow = "none";
+        keys[3].style.border = "3px solid black";
+        keys[3].style.transform = "scale(1)";
+        keys[3].style.boxShadow = "none";
+        keys[4].style.border = "3px solid black";
+        keys[4].style.transform = "scale(1)";
+        keys[4].style.boxShadow = "none";
+        keys[5].style.border = "3px solid black";
+        keys[5].style.transform = "scale(1)";
+        keys[5].style.boxShadow = "none";
+        keys[6].style.border = "3px solid black";
+        keys[6].style.transform = "scale(1)";
+        keys[6].style.boxShadow = "none";
+        keys[7].style.border = "3px solid black";
+        keys[7].style.transform = "scale(1)";
+        keys[7].style.boxShadow = "none";
+        keys[8].style.border = "3px solid black";
+        keys[8].style.transform = "scale(1)";
+        keys[8].style.boxShadow = "none";
+    }
+    setTimeout(lightOut, 100);
     if(e.keyCode === 65 && keys[0]){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/clap.wav");
         Audio.play();
-        keys[0].classList.add("playing")
-        keys[0].classList.remove("each")
         // let keys = document.querySelector(".eachkey");
-        console.log(keys[0].classList);
         keys[0].style.border = "3px solid yellow";
         keys[0].style.transform = "scale(1.1)";
         keys[0].style.boxShadow = "0 0 10px #ffc600";
@@ -19,49 +46,73 @@ window.addEventListener("keydown", function(e){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/hihat.wav");
         Audio.play();
-        keys[1].classList.add("opening");    }
+        keys[1].style.border = "3px solid yellow";
+        keys[1].style.transform = "scale(1.1)";
+        keys[1].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY C
     else if(e.keyCode === 67){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/kick.wav");
         Audio.play();
-        keys[2].classList.add("opening");    }
+        keys[2].style.border = "3px solid yellow";
+        keys[2].style.transform = "scale(1.1)";
+        keys[2].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY D
     else if(e.keyCode === 68){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/openhat.wav");
         Audio.play();
-        keys[3].classList.add("opening");    }
+        keys[3].style.border = "3px solid yellow";
+        keys[3].style.transform = "scale(1.1)";
+        keys[3].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY E
     else if(e.keyCode === 69){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/boom.wav");
         Audio.play();
-        keys[4].classList.add("opening");}
+        keys[4].style.border = "3px solid yellow";
+        keys[4].style.transform = "scale(1.1)";
+        keys[4].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY F
     else if(e.keyCode === 70){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/ride.wav");
         Audio.play();
-        keys[5].classList.add("opening");    }
+        keys[5].style.border = "3px solid yellow";
+        keys[5].style.transform = "scale(1.1)";
+        keys[5].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY G
     else if(e.keyCode === 71){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/snare.wav");
         Audio.play();
-        keys[6].classList.add("opening");    }
+        keys[6].style.border = "3px solid yellow";
+        keys[6].style.transform = "scale(1.1)";
+        keys[6].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY H
     else if(e.keyCode === 72){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/tom.wav");
         Audio.play();
-        keys[7].classList.add("opening");    }
+        keys[7].style.border = "3px solid yellow";
+        keys[7].style.transform = "scale(1.1)";
+        keys[7].style.boxShadow = "0 0 10px #ffc600";
+    }
         // KEY I
     else if(e.keyCode === 73){
         let Audio = document.createElement("audio");
         Audio.setAttribute("src", "audio/tink.wav");
         Audio.play();
-        keys[8].classList.add("opening");    }
+        keys[8].style.border = "3px solid yellow";
+        keys[8].style.transform = "scale(1.1)";
+        keys[8].style.boxShadow = "0 0 10px #ffc600";
+    }
         else{
             return;
         }
